ce981f0d33b3e9cc17a524ffcc7dd85fcacefa7c
Belceb45 <rubiodiego001@gmail.com>
2026-08-09 23:28:07 -0600

    cambios ruta api

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,11 +1,11 @@
 import { API_BASE_URL } from "./config";
 
 export const USER_API = {
-    REGISTER: `${API_BASE_URL}/users/register`,
-    LOGIN: `${API_BASE_URL}/users/login`,
-    UPDATE: (id: string) => `${API_BASE_URL}/users/actualizar/${id}`,
-    UPDATEDOCTOR:(id:string)=>`${API_BASE_URL}/users/actualizar/doctor/${id}`,
-    RESULTS:(id:string)=>`${API_BASE_URL}/resultados/paciente/${id}`,
-    HISTORIAL:(id:string)=>`${API_BASE_URL}/historial/${id}`,
-    GUARDAR_HISTORIAL:`${API_BASE_URL}/historial/guardar`
+    REGISTER: `${API_BASE_URL}/api/users/register`,
+    LOGIN: `${API_BASE_URL}/api/users/login`,
+    UPDATE: (id: string) => `${API_BASE_URL}/api/users/actualizar/${id}`,
+    UPDATEDOCTOR:(id:string)=>`${API_BASE_URL}/api/users/actualizar/doctor/${id}`,
+    RESULTS:(id:string)=>`${API_BASE_URL}/api//resultados/paciente/${id}`,
+    HISTORIAL:(id:string)=>`${API_BASE_URL}/api//historial/${id}`,
+    GUARDAR_HISTORIAL:`${API_BASE_URL}/api//historial/guardar`
 }
